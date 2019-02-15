31661ef8cb33e4d0b29d0af50983dd3bd4b27564
Martín Figueroa Padilla <martnfigueroa21@gmail.com>
2019-02-15 12:09:31 -0600

    Memorama 3

--- app/src/main/java/com/example/memorama/MemoramaAdapter.kt
+++ app/src/main/java/com/example/memorama/MemoramaAdapter.kt
@@ -2,10 +2,22 @@ package com.example.memorama
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.view.ViewGroup
 
-class MemoramaAdapter(val chips : ArrayList<Chip>) {
+class MemoramaAdapter(val chips : ArrayList<Chip>) : RecyclerView.Adapter<MemoramaAdapter.ChipViewHolder>() {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ChipViewHolder {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(p0: ChipViewHolder, p1: Int) {
+
+    }
 
     inner class ChipViewHolder(item : View) : RecyclerView.ViewHolder(item) {
-        
+
     }
 }
