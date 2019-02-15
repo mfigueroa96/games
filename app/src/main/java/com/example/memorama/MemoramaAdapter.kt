2102760a372b28f12baa6eab5100d89bae72d974
Martín Figueroa Padilla <martnfigueroa21@gmail.com>
2019-02-15 12:02:15 -0600

    Memorama otra vez

--- app/src/main/java/com/example/memorama/MemoramaAdapter.kt
+++ app/src/main/java/com/example/memorama/MemoramaAdapter.kt
@@ -1,5 +1,11 @@
 package com.example.memorama
 
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
 class MemoramaAdapter(val chips : ArrayList<Chip>) {
 
+    inner class ChipViewHolder(item : View) : RecyclerView.ViewHolder(item) {
+        
+    }
 }
