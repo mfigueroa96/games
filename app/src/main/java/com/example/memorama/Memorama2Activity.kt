126fdd0fe190b9b069ab21f893d81080b834f238
Martín Figueroa Padilla <martnfigueroa21@gmail.com>
2019-02-19 11:45:24 -0600

    Oks pero no aparece nada

---
+++ app/src/main/java/com/example/memorama/Memorama2Activity.kt
@@ -0,0 +1,27 @@
+package com.example.memorama
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import com.example.games.R
+import kotlinx.android.synthetic.main.activity_memorama2.*
+
+class Memorama2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memorama2)
+        val rv = recyclerView1
+        rv.setHasFixedSize(true)
+        var grid = GridLayoutManager(this, 4)
+        rv.layoutManager = grid
+
+        val chips = ArrayList<Chip>()
+        for (i in 1..12) {
+            chips.add(Chip(R.mipmap.ic_launcher))
+        }
+
+        var adapter = MemoramaAdapter(chips)
+        rv.adapter = adapter
+    }
+}
--- app/src/main/java/com/example/memorama/MemoramaAdapter.kt
+++ app/src/main/java/com/example/memorama/MemoramaAdapter.kt
@@ -1,23 +1,33 @@
 package com.example.memorama
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.example.games.R
 
 class MemoramaAdapter(val chips : ArrayList<Chip>) : RecyclerView.Adapter<MemoramaAdapter.ChipViewHolder>() {
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ChipViewHolder {
-
+        val view = LayoutInflater.from(p0.context).inflate(R.layout.row, p0, false)
+        return ChipViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-
+        return chips.size
     }
 
     override fun onBindViewHolder(p0: ChipViewHolder, p1: Int) {
-
+        p0.imageView.setImageResource(chips[p1].idImg)
     }
 
     inner class ChipViewHolder(item : View) : RecyclerView.ViewHolder(item) {
+        val imageView = item.findViewById<ImageView>(R.id.chip)
 
+        init {
+            item.setOnClickListener {
+                imageView.setImageResource(R.drawable.doggo)
+            }
+        }
     }
 }
